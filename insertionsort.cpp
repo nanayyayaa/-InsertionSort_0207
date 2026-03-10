f54fad490c0bf9d6f29e165d949d7d93e0688a8d
nanayyayaa <jiejueejuju@gmail.com>
2026-03-10 12:02:32 +0700

    procedure_display2

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -53,4 +53,10 @@ void display()
     cout << "\n=================";
     cout << "\nElement Array" << endl;
     cout << "\n================="; << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
 }
